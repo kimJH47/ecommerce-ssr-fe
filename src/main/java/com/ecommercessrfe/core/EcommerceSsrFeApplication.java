9e32f4804859b753c9c52367d6d26e6df75391ba
kimJaeHyeon <kmr2644@gmail.com>
2023-12-04 18:26:22 +0900

    init

---
+++ src/main/java/com/ecommercessrfe/core/EcommerceSsrFeApplication.java
@@ -0,0 +1,13 @@
+package com.ecommercessrfe.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSsrFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceSsrFeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommercessrfe/core/EcommerceSsrFeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommercessrfe.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceSsrFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
