e6e579a1a423d580bf14341474626dbfcd845aea
kimJaeHyeon <kmr2644@gmail.com>
2023-12-12 03:40:10 +0900

    feat: message-redirect 구현

--- src/main/java/com/ecommercessrfe/core/controller/web/HomeController.java
+++ src/main/java/com/ecommercessrfe/core/controller/web/HomeController.java
@@ -1,15 +1,13 @@
 package com.ecommercessrfe.core.controller.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.ecommercessrfe.core.mapper.CartAppendModel;
 import com.ecommercessrfe.core.mapper.CartModel;
-
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/")
@@ -21,17 +19,11 @@ public class HomeController {
 		return "home";
 	}
 
-	@GetMapping("redirect/success-login")
-	public String successLoginRedirect(@ModelAttribute("token") String token, HttpServletResponse httpServletResponse) {
-		httpServletResponse.addCookie(createTokenCookie(token));
-		return "redirect:/";
-	}
-
-	private Cookie createTokenCookie(String token) {
-		Cookie cookie = new Cookie("token", token);
-		cookie.setHttpOnly(true);
-		cookie.setMaxAge(60 * 60 * 3); //3 hour
-		cookie.setPath("/");
-		return cookie;
+	@GetMapping("/message-redirect")
+	public String messageRedirect(@ModelAttribute("message") String message,
+		@ModelAttribute("redirectUrl") String redirectUrl, Model model) {
+		model.addAttribute("message", message);
+		model.addAttribute("redirectUrl", redirectUrl);
+		return "messageRedirect";
 	}
 }
