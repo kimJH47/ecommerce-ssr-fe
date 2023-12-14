2e21a25dea5c70bead250292d6940037a893173b
kimJaeHyeon <kmr2644@gmail.com>
2023-12-14 22:58:28 +0900

    feat: page 조회 구현

--- src/main/java/com/ecommercessrfe/core/controller/web/ProductController.java
+++ src/main/java/com/ecommercessrfe/core/controller/web/ProductController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.ecommercessrfe.core.mapper.CartAppendModel;
 import com.ecommercessrfe.core.mapper.CartModel;
@@ -23,17 +24,23 @@ public class ProductController {
 	}
 
 	@GetMapping("/{category}")
-	public String categories(@PathVariable("category") String category, Model model) {
+	@CartAppendModel
+	public String categories(@PathVariable("category") String category, Model model, CartModel cartModel) {
 		Response<ProductListDto> response = productService.findByCategory(category);
 		model.addAttribute("products", response.getEntity().getProducts());
-		return "categories/categories-view";
+		model.addAttribute("page", 1);
+		model.addAttribute("category", category.toUpperCase());
+		return "categories/test";
 	}
 
-	@GetMapping("/test")
+	@GetMapping("/{category}/detail")
 	@CartAppendModel
-	public String test(Model model, CartModel cartModel) {
-		Response<ProductListDto> response = productService.findByCategory("TOP");
+	public String page(@RequestParam("page") String page, @PathVariable("category") String category, Model model,
+		CartModel cartModel) {
+		Response<ProductListDto> response = productService.findByCategoryWithPagination(category, page);
 		model.addAttribute("products", response.getEntity().getProducts());
+		model.addAttribute("page", page);
+		model.addAttribute("category", category);
 		return "categories/test";
 	}
 }
--- src/main/java/com/ecommercessrfe/core/service/ProductService.java
+++ src/main/java/com/ecommercessrfe/core/service/ProductService.java
@@ -1,9 +1,12 @@
 package com.ecommercessrfe.core.service;
 
+import java.net.URI;
+
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ecommercessrfe.core.service.dto.ProductListDto;
 import com.ecommercessrfe.core.service.dto.Response;
@@ -20,6 +23,27 @@ public class ProductService {
 	public Response<ProductListDto> findByCategory(String category) {
 		return restClient.get()
 			.uri("https://kecommerce.shop/api/categories/" + category)
+			.accept(MediaType.APPLICATION_JSON)
+			.exchange((request, response) -> {
+				if (response.getStatusCode().is4xxClientError()) {
+					return new Response<ProductListDto>("조회된 상품 없음 ", ProductListDto.emptyList());
+				}
+				return response.bodyTo(new ParameterizedTypeReference<Response<ProductListDto>>() {
+				});
+			});
+	}
+
+	public Response<ProductListDto> findByCategoryWithPagination(String category, String page) {
+		URI url = UriComponentsBuilder.newInstance()
+			.scheme("https")
+			.host("kecommerce.shop")
+			.path("/api/categories/" + category + "/detail")
+			.queryParam("page", page)
+			.build()
+			.toUri();
+		url = url;
+		return restClient.get()
+			.uri(url)
 			.accept(MediaType.APPLICATION_JSON)
 			.exchange((request, response) -> {
 				if (response.getStatusCode().is4xxClientError()) {
